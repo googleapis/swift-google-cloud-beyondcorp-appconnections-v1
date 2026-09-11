8247231452b51a627eb93badf91d6055e0929d39
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudBeyondCorpAppConnectionsV1/AppConnection.swift
+++ Sources/GoogleCloudBeyondCorpAppConnectionsV1/AppConnection.swift
@@ -241,8 +241,8 @@ public struct AppConnection: Codable, Equatable, GoogleCloudWKT._AnyPackable,
       public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         switch self {
-        case .unspecified: return try container.encode(0)
-        case .gcpRegionalMig: return try container.encode(1)
+        case .unspecified: return try container.encode("TYPE_UNSPECIFIED")
+        case .gcpRegionalMig: return try container.encode("GCP_REGIONAL_MIG")
         case .unknownIntValue(let v): return try container.encode(v)
         case .unknownStringValue(let v): return try container.encode(v)
         }
@@ -352,8 +352,8 @@ public struct AppConnection: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .tcpProxy: return try container.encode(1)
+      case .unspecified: return try container.encode("TYPE_UNSPECIFIED")
+      case .tcpProxy: return try container.encode("TCP_PROXY")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
@@ -475,12 +475,12 @@ public struct AppConnection: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .creating: return try container.encode(1)
-      case .created: return try container.encode(2)
-      case .updating: return try container.encode(3)
-      case .deleting: return try container.encode(4)
-      case .down: return try container.encode(5)
+      case .unspecified: return try container.encode("STATE_UNSPECIFIED")
+      case .creating: return try container.encode("CREATING")
+      case .created: return try container.encode("CREATED")
+      case .updating: return try container.encode("UPDATING")
+      case .deleting: return try container.encode("DELETING")
+      case .down: return try container.encode("DOWN")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
